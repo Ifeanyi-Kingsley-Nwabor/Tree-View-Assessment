1bd6d5cef8b9c792049ccdedfddc17334919cd74
Codify KING <81686044+Ifeanyi-Kingsley-Nwabor@users.noreply.github.com>
2022-04-29 00:24:14 +0200

    first commit

---
+++ src/components/DisplayPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <button @click="isHidden = !isHidden">
+      {{ isHidden ? "Show Message" : "Hide Message" }}
+    </button>
+
+    <h1 v-if="!isHidden">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisplayPage",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      isHidden: false,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DisplayPage msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DisplayPage from "@/components/DisplayPage.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    DisplayPage,
+  },
+};
 </script>
